eef8f3b6ff33bb86ab6363a616953220cbdda802
antony <antony.locateli@escola.pr.gov.br>
2026-04-26 22:43:38 -0300

    MDS

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 const mysql = require("mysql");
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(express.json());
+app.use(cors());
 
 const connection = mysql.createConnection({
     host:"localhost",
@@ -15,18 +21,45 @@ connection.connect((Error) => {
     console.log("conexao boa");
 })
 
-connection.query('SELECT * FROM alunos', (Error, results) =>{
-    if(Error) {
-        console.log("erro", Error);
-        return
-    }
-    console.log("resultado ", results);
-});
-
-connection.end((Error) => {
-    if (Error){ 
-    console.error("erro ao fechar CX" , Error);
-    return;
-    }
-    console.log("CX fechada");
+app.post("/cadastrar", (req, res) =>{
+    const {nome, senha} = req.body;
+    const sql = "INSERT INTO cadastro_professor (`Nome`, `Senha`) VALUES(?, ?)";
+    connection.query(sql, [nome, senha], (err) => {
+        if(err){ 
+            console.error(err)
+            return res.status(500).json({mensagem: "usuario ja existente"});
+        }
+        res.json({mensagem: "professor cadastrado"});
+    })
 })
+
+app.post("/login", (req, res) => {
+    const { usuario, senha} = req.body
+    const sql = "SELECT * FROM cadastro_professor WHERE `Nome` = ? AND `Senha` = ?"
+    connection.query(sql, [usuario, senha], (err, results) => {
+        if (results.length > 0) {
+            res.json({mensagem: "aprovado"})
+        }
+        else{
+            res.status(401).json({mensagem:"incorreto"})
+        }
+    })
+})
+
+app.post("/registrar-ocorrencia", (req, res) =>{
+    const { aluno, turma, professor, data, motivo} = req.body;
+
+    const query = "INSERT INTO `ocorrências` (motivo, aluno_envolvido,professor_envolvido, data_ocorrencia,turma ) VALUES(?,?,?,?,?) ";
+
+    connection.query(query, [motivo, aluno,professor, data,turma ], (err, result) =>{
+        if(err){
+            console.error(err);
+            return res.status(500).send("erro ao salvar");
+
+        }
+        res.send({mensagem:"ocorrencia salva"})
+    })
+})
+
+app.listen(1200, () => console.log("sv porta 1200"))
+
